e6324d4869406c9b8fa55262af0d407f4822a382
zenos-pavlakou <zenos_pavlakou@outlook.com>
2024-11-23 16:31:18 +0200

    fixed api

--- db_models.py
+++ db_models.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, Foreign
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
 
-from . import config
-
 __all__ = ["User"]
 
 class Base(DeclarativeBase):
@@ -14,12 +12,3 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     is_manager = Column(Boolean, default=True)
-
-def init_db():
-    engine = create_engine(config.DB_URL)
-    Base.metadata.create_all(engine)
-    return engine
-
-# Add this line at the bottom to execute the function
-if __name__ == "__main__":
-    engine = init_db()
